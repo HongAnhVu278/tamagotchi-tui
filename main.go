d5e8b7e5c2814143b17dfb8b9437af6f9761c08f
Anh Vu <honganhvu278@gmail.com>
2026-07-16 12:19:20 -0400

    clean up main

--- main.go
+++ main.go
@@ -19,55 +19,79 @@ const (
 	threshold   = 86400 //5 days
 )
 
+func runTui() error {
+	data, err := loadDataFromFile()
+
+	if err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+
+	p := tea.NewProgram(initialModel(data))
+
+	if _, err := p.Run(); err != nil {
+		return fmt.Errorf("there's been an error: %w", err)
+	}
+	return nil
+}
+
+func runSpeak() error {
+	speakLine, err := speak()
+
+	if err != nil {
+		return fmt.Errorf("speak: %w\n", err)
+	}
+
+	fmt.Println(speakLine)
+	return nil
+
+}
+
+func runGit() error {
+	cmd := exec.Command("git", os.Args[1:]...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	err := appendLine("commits.log", fmt.Sprintf("%d\n", time.Now().Unix()))
+	if err != nil {
+		return fmt.Errorf("record commit: %w\n", err)
+	}
+
+	return nil
+
+}
+
 func main() {
+	var err error
 	if len(os.Args) < 2 {
-		data, err := loadDataFromFile()
+		err = runTui()
 
 		if err != nil {
-			fmt.Println("error loading model", err)
-			os.Exit(1)
-		}
-
-		p := tea.NewProgram(initialModel(data))
-
-		if _, err := p.Run(); err != nil {
-			fmt.Printf("there's been an error: %v", err)
+			fmt.Println(err)
 			os.Exit(1)
 		}
 		return
-
 	}
+
 	switch os.Args[1] {
 	case "speak":
-		speakLine, err := speak()
+		err = runSpeak()
 
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "speak: %v\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Println(speakLine)
-		return
 	default:
-		cmd := exec.Command("git", os.Args[1:]...)
+		err = runGit()
+	}
 
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		if err := cmd.Run(); err != nil {
-			if exitErr, ok := err.(*exec.ExitError); ok {
-				os.Exit(exitErr.ExitCode())
-			}
-			os.Exit(1)
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
 		}
-
-		err := appendLine("commits.log", fmt.Sprintf("%d\n", time.Now().Unix()))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "record commit: %v\n", err)
-			os.Exit(1)
-		}
-
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 }
